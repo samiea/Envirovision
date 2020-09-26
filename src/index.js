6dd90b2e8acfe61f6622231487a3ef7c9119db55
Samie Amriui <samiea@vt.edu>
2020-09-26 00:29:32 -0400

    Didn't seem to apply deletions

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
+import axios from 'axios'; 
 
 
 var startCarbonDioxide=function(carbonArray){
