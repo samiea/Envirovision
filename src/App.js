e657f0eec3088c259c14d167613d88529e2b802f
rachelhachem <rachelly@vt.edu>
2020-09-29 09:54:19 -0400

    Making my branch match Samie's

--- src/App.js
+++ src/App.js
@@ -1,26 +1,82 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Child1 from './Child1' // import both children
+import Child2 from './Child2'
+import axios from 'axios'; // used for API stuff
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// below imports are for static files to avoid 429 errors from requests while testing
+import carbonJSON from './static/carbon.json'
+import methaneJSON from './static/methane.json'
+import nitrousJSON from './static/nitrous.json'
+import temperatureJSON from './static/temperature.json'
+// end testing-related import block
+
+class App extends React.Component {
+    // you can create class-scope fields in here like in Java
+    constructor(props) {
+        super(props)
+        this.handleQuarterChange = this.handleQuarterChange.bind(this); // notice that functions need to be binded
+        this.handleCountyChange = this.handleCountyChange.bind(this);
+        this.state = { // you can add new states here
+            // initialize below states to null if not using static data for testing
+            carbonData: carbonJSON.co2,
+            methaneData: methaneJSON.methane,
+            nitrousData: nitrousJSON.nitrous,
+            temperatureData: temperatureJSON.result,
+            dataIsLoaded: false
+        };
+    }
+
+    handleQuarterChange(value) {
+        this.setState({ quarter: value });
+    }
+
+    handleCountyChange(value) {
+        this.setState({ county: value });
+    }
+
+    loadData() {
+        const fetchData = async () => {
+            const proxyurl = "https://cors-anywhere.herokuapp.com/"; // proxy url that is used in combination with real url
+            
+            let promises = []; // make an array of promises
+            this.props.urls.split(",").forEach(function(url) {
+                promises.push(axios(proxyurl + url)); // push request onto promise array
+            })
+            
+            // order of promises is retained; reference: https://stackoverflow.com/questions/28066429/promise-all-order-of-resolved-values/28066851
+            const data = await Promise.all(promises);
+
+            this.state.carbonData = data[0].data.co2; // directly modifying the state like this does NOT force re-render
+            this.state.methaneData = data[1].data.methane;
+            this.state.nitrousData = data[2].data.nitrous;
+            this.state.temperatureData = data[3].data.result;
+
+            this.setState({ dataIsLoaded: true }); // calling this.setState(...) forces re-render
+        };
+        fetchData();
+    }
+
+    componentDidMount() { // this is called when the page is initially loaded/mounted
+        console.log("Parent Mounted");
+        // this.loadData(); // comment this out if using static files; loadData() will make requests
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) { // when re-render occurs, componentDidUpdate() is called
+        console.log("Parent Updated");
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Child1
+                    carbonData={this.state.carbonData} // need to pass data into children via props
+                />
+                <Child2
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,86 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import axios from 'axios';
-import P5Wrapper from 'react-p5-wrapper';
-import sketch from './sketches/sketch';
 
-let loaded = false;
-let carbData = [];
-let metData = [];
-let iceData = [];
-let nitrData = [];
-let tempData = [];
-
-const pullData = async () => {
-
-  function getApibyUrl(url, dataArray){
-
-    const proxyurl = "https://cors-anywhere.herokuapp.com/";
-
-    var response = axios.get(
-      proxyurl+url
-    ).then(function(response){
-        //console.log(response.data)
-        dataArray = response.data;
-        console.log("dataArray: ");
-        console.log(dataArray);
-        return dataArray;
-    }).catch(err => {
-        // what now?
-      console.log(err);
-    });
-
-  }
-
-  //Air
-  var url = 'https://global-warming.org/api/co2-api'
-  getApibyUrl(url, carbData);
-
-  url = 'https://global-warming.org/api/methane-api'
-
-  url = 'https://global-warming.org/api/nitrous-oxide-api'
-
-  //surface temperature
-  url = 'https://global-warming.org/api/temperature-api'
-
-  //arctic ice
-  url = 'https://global-warming.org/api/arctic-api'
-};
-
-function Environment() {
-
-  return (
-      <div className="App">
-        <h1>Air Quality</h1>
-        <h2>Fetch a list from an API and display it</h2>
-
-        /* Fetch data from API */
-        <div>
-          <button className="fetch-button" onClick={pullData}>
-            Pull Data
-          </button>
-          
-          <P5Wrapper sketch={sketch} carbon={carbData}></P5Wrapper>
-
-          <br />
-        </div>
-
-      </div>
-  );
-}
-
-//start application
-
-const rootElement = document.getElementById('root');
-
-pullData().then(function(response){
-    loaded = true;
-    console.log("data loaded");
-    ReactDOM.render(<Environment />, rootElement);
-  }
+// there is probably a way better way to do this, but new urls are added to end of the urls csv string
+ReactDOM.render(
+    <React.StrictMode>
+        <App
+            // urls={"https://global-warming.org/api/co2-api,https://global-warming.org/api/methane-api,https://global-warming.org/api/nitrous-oxide-api,https://global-warming.org/api/temperature-api"}
+        />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
-//ReactDOM.render(<Environment />, rootElement);
+serviceWorker.unregister();
