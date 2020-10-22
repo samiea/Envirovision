b5c552a00822d8e6bd06b3481d586534f0a7ef5f
qdeggles <qdeggles@vt.edu>
2020-10-22 00:03:14 -0400

    simple wipedown transition proof of concept

--- src/sketches/sketch.js
+++ src/sketches/sketch.js
@@ -2,10 +2,21 @@ import { drawSun } from './sun';
 import { setupLandscape, drawLandscape } from './landscape';
 import { setupDrops, drawDots } from './microPlastic'
 
+let skyView = false;
+let wipeDownExpand = false;
+let wipeDownContract = false;
+
+function skySwap() {
+    skyView = !skyView;
+    wipeDownExpand = true;
+}
+
 export default function sketch(p) {
     let temperatureData = null;
     let microGrowth2050 = null;
     let currentDate = null;
+    let transitionY = 0;
+    let transitionX = 0;
 
     p.setup = () => {
         p.frameRate(30);
@@ -15,8 +26,29 @@ export default function sketch(p) {
     }
 
     p.draw = () => {
-        drawLandscape(p, temperatureData, currentDate);
-        drawDots(p,microGrowth2050,currentDate);
+        if(!skyView) {
+            drawLandscape(p, temperatureData, currentDate);
+            drawDots(p,microGrowth2050,currentDate);
+        }
+        else if(wipeDownExpand) {
+            drawLandscape(p, temperatureData, currentDate);
+            drawDots(p,microGrowth2050,currentDate);
+            p.fill(0);
+            p.rect(transitionX, 0, p.width, transitionY);
+            transitionY += 5;
+            if(transitionY >= p.height) {
+                wipeDownExpand = false;
+                wipeDownContract = true;
+            }
+        }
+        else if(wipeDownContract) {
+            drawLandscape(p, temperatureData, currentDate);
+            drawDots(p,microGrowth2050,currentDate);
+            p.fill(0);
+            p.rect(transitionX, p.height - transitionY, p.width, transitionY);
+            transitionY -= 5;
+            console.log("contracting");
+        }
     }
 
     p.myCustomRedrawAccordingToNewPropsHandler = (newProps) => {
@@ -24,4 +56,10 @@ export default function sketch(p) {
         currentDate = newProps.currentDate;
         microGrowth2050 = newProps.microGrowth2050;
     }
+
+    p.mouseClicked = () => {
+        skySwap();
+    }
+
 }
+
